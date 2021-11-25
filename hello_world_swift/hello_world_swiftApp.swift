673cfef7804aa71e3250f29cebf4a519caf2cff2
Terje Christensen <terje@christensen.no>
2021-11-25 07:51:38 +0100

    Initial Commit

---
+++ hello_world_swift/hello_world_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_world_swiftApp.swift
+//  hello_world_swift
+//
+//  Created by Terje Christensen on 25/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct hello_world_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
